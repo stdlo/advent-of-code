75edf37724a208fe5a5d02798b423d77210cf1dd
lbickmore <loganbickmore91@gmail.com>
2017-12-01 13:17:47 -0800

    deleted readme, prepped day2

---
+++ 2017/2/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/loganbickmore/advent-of-code/utils"
+)
+
+var inputStr string
+var length int
+
+func part1() {
+	//
+}
+func part2() {
+	//
+}
+func main() {
+
+	inputStr = utils.GetInput()
+	length = len(inputStr)
+
+	fmt.Println("inputStr =>", inputStr)
+}
