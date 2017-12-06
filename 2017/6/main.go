d0aaa1f5de8cb09558bad7776a242272cf0a56d4
lbickmore <loganbickmore91@gmail.com>
2017-12-06 12:48:50 -0800

    solved day 6

--- 2017/6/main.go
+++ 2017/6/main.go
@@ -3,45 +3,40 @@ package main
 import (
 	"fmt"
 	"sort"
-	"strings"
 )
 
 type matrix [][]int
 
 var (
 	test  = []int{0, 2, 7, 0}
-	brad  = []int{11, 11, 13, 7, 0, 15, 5, 5, 4, 4, 1, 1, 7, 1, 15, 11}
 	input = []int{4, 1, 15, 12, 0, 9, 9, 5, 5, 8, 7, 3, 14, 5, 12, 3}
+	match = 0
 )
 
 func main() {
-	fmt.Println("test ->", loop(test, matrix{}))
-	return
-	fmt.Println("part1 ->", loop(input, matrix{}))
+	fmt.Println("test ->", solve(test, matrix{}))
+
+	results := solve(input, matrix{})
+	fmt.Println("part1 ->", results)
+	fmt.Println("part2 ->", results-match)
 }
 
-func loop(blocks []int, encountered matrix) int {
-	// mark as previously seen
+func solve(blocks []int, encountered matrix) int {
 	encountered = markAsSeen(blocks, encountered)
-
-	//fmt.Println("e", encountered)
-	//fmt.Println("b", blocks)
 
 	blocks = redistrubute(blocks)
 
-	// check for previously seen
 	if previouslySeen(blocks, encountered) {
 		return len(encountered)
 	}
 
-	return loop(blocks, encountered)
+	return solve(blocks, encountered)
 }
 
 func previouslySeen(blocks []int, encountered matrix) bool {
-	for _, encounteredBlocks := range encountered {
-		if IntArrEq(blocks, encounteredBlocks) {
-			//fmt.Println(blocks, "evolution", i)
-			//fmt.Println(encounteredBlocks)
+	for i, encounteredBlocks := range encountered {
+		if intArrEq(blocks, encounteredBlocks) {
+			match = i
 			return true
 		}
 	}
@@ -52,39 +47,6 @@ func markAsSeen(blocks []int, encountered matrix) matrix {
 	var copyOfBlocks = make([]int, len(blocks))
 	copy(copyOfBlocks, blocks)
 	return append(encountered, copyOfBlocks)
-}
-
-func getHighestBlock(blocks []int) (int, int) {
-	var index int
-	var sorted = make([]int, len(blocks))
-	copy(sorted, blocks)
-	sort.Ints(sorted)
-	big := sorted[len(blocks)-1]
-	for i, n := range blocks {
-		if n == big {
-			index = i
-			return blocks[index], index
-		}
-	}
-	return blocks[index], index
-}
-
-func IntArrEq(a, b []int) bool {
-	if a == nil && b == nil {
-		return true
-	}
-	if a == nil || b == nil {
-		return false
-	}
-	if len(a) != len(b) {
-		return false
-	}
-	for i := range a {
-		if a[i] != b[i] {
-			return false
-		}
-	}
-	return true
 }
 
 func redistrubute(blocks []int) []int {
@@ -102,6 +64,35 @@ func redistrubute(blocks []int) []int {
 	return blocks
 }
 
-func arrToS(a []int, delim string) string {
-	return strings.Trim(strings.Replace(fmt.Sprint(a), " ", delim, -1), "[]")
+func getHighestBlock(blocks []int) (int, int) {
+	var index int
+	var sorted = make([]int, len(blocks))
+	copy(sorted, blocks)
+	sort.Ints(sorted)
+	big := sorted[len(blocks)-1]
+	for i, n := range blocks {
+		if n == big {
+			index = i
+			return blocks[index], index
+		}
+	}
+	return blocks[index], index
+}
+
+func intArrEq(a, b []int) bool {
+	if a == nil && b == nil {
+		return true
+	}
+	if a == nil || b == nil {
+		return false
+	}
+	if len(a) != len(b) {
+		return false
+	}
+	for i := range a {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+	return true
 }
