562657b79dc8844f4570b00a406f69e16ca0542f
Logan Bickmore <loganbickmore91@gmail.com>
2019-11-22 13:59:18 -0800

    added utils.jl, updated 201802 to use it

--- 2018/main.02.jl
+++ 2018/main.02.jl
@@ -1,44 +1,30 @@
 #! /usr/bin/env julia
 using Test
+include("../utils.jl"); using ..Utils
 
-# return array of data split on newlines from file path
-load_data = (file_path) -> open(expanduser(file_path)) do f readlines(f) end
-partial = (f, a...) -> (b...) -> f(a..., b...)
-function tap(fn, x) fn(x); return x end
-logtap = partial(tap, println)
-
-case = [[
-"abcdef",# contains no letters that appear exactly two or three times.
-"bababc",# contains two a and three b, so it counts for both.
-"abbcde",# contains two b, but no letter appears exactly three times.
-"abcccd",# contains three c, but no letter appears exactly two times.
-"aabcdd",# contains two a and two d, but it only counts once.
-"abcdee",# contains two e.
-"ababab",# contains three a and three b, but it only counts once.
-], 12]
-
-
+pick2and3 = partial(filter, x-> !(x<2 || x>3))
 function inc_occr(list, d = Dict()) foreach(a->haskey(d,a) ? d[a] += 1 : d[a] = 1, list); return d end
-function recurse(arr, d = Dict())
+
+function solve_v1(arr, d = Dict())
   len = length(arr)
   return  len == 0 ?
   reduce(*, values(d)) :
-  recurse(arr[2:len],
-          inc_occr(filter(x-> !(x<2 || x>3), unique(values(inc_occr(arr[1])))), d))
+  solve_v1(arr[2:len],
+          inc_occr(pick2and3(unique(values(inc_occr(arr[1])))), d))
 end
-my_filter = partial(filter, x-> !(x<2 || x>3))
-flatten = a -> collect(Iterators.flatten(a))
-solve = list -> (map(my_filter ∘ unique ∘ values ∘ inc_occr, list) 
+
+solve_v2 = list -> (map(pick2and3 ∘ unique ∘ values ∘ inc_occr, list) 
                  |> flatten 
                  |> inc_occr 
                  |> values 
                  |> partial(reduce, *)
                 )
-@test recurse(case[1]) == case[2]
-@test solve(case[1]) == case[2]
+
+test_case = [["abcdef","bababc","abbcde","abcccd","aabcdd","abcdee","ababab"], 12]
+@test solve_v1(test_case[1]) == test_case[2]
+@test solve_v2(test_case[1]) == test_case[2]
 
 file_path = try ARGS[1] catch; "input.02.txt" end
-data = load_data(file_path)
-
-println(solve(data))
+data = read_file_lines(file_path)
+println(solve_v2(data))
 
---
+++ utils.jl
@@ -0,0 +1,15 @@
+module Utils
+export partial, read_file_lines, read_file_slurp, tap, logtap, flatten
+
+# return array of data split on newlines from file path
+read_file_lines = (file_path) -> open(expanduser(file_path)) do f readlines(f) end
+read_file_slurp = (file_path) -> open(expanduser(file_path)) do f read(f, String) end
+
+partial = (f, a...) -> (b...) -> f(a..., b...)
+
+function tap(fn, x) fn(x); return x end
+logtap = partial(tap, println)
+
+flatten = a -> collect(Iterators.flatten(a))
+
+end
