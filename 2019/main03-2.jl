d27f48201db1bf7bc0a23b3d13e55e996abed6d4
Logan Bickmore <loganbickmore91@gmail.com>
2019-12-20 12:43:30 -0800

    2019-03 p2 complete. p1 cleaned

--- 2019/main03-2.jl
+++ 2019/main03-2.jl
@@ -32,13 +32,18 @@ function plot(path)
   return coords
 end
 
+function count_steps(path,target)
+  for i in 1:length(path)
+    if path[i]==target
+      return i - 1
+    end
+  end
+end
+
 function transform(wa,wb)
-  # take a vertex and add it together then abs it to get the manhattan distance
-  # also abs both points and get the mean to find which point is actually closest to 0,0
   return function(vertex)
-    mdist = abs(vertex[1] + vertex[2])
-    mean = (abs(vertex[1]) + abs(vertex[2])) / 2
-    return (vertex, mdist, mean)
+    steps = count_steps(wa,vertex)+count_steps(wb,vertex)
+    return (vertex, steps)
   end
 end
 
@@ -47,10 +52,10 @@ function solve(wires)
   wb = plot(split(wires[2],","))
   intersections = map(transform(wa,wb), intersect(wa,wb)[2:end])
 
-  # loop over intersections and get the lowest abs mean from intersections
   result=intersections[1]
   for intersection in intersections
-    if result[3] > intersection[3]
+    # if result steps > intersection steps
+    if result[2] > intersection[2]
       result=intersection
     end
   end
@@ -58,9 +63,9 @@ function solve(wires)
 end
 
 test_cases = [
-  [["R8,U5,L5,D3",                                "U7,R6,D4,L4"],                         6],
-  [["R75,D30,R83,U83,L12,D49,R71,U7,L72",         "U62,R66,U55,R34,D71,R55,D58,R83"],     159],
-  [["R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51","U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"],135],
+  [["R8,U5,L5,D3",                                "U7,R6,D4,L4"],                         30],
+  [["R75,D30,R83,U83,L12,D49,R71,U7,L72",         "U62,R66,U55,R34,D71,R55,D58,R83"],     610],
+  [["R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51","U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"],410],
   ]
 
 for case in test_cases
@@ -70,5 +75,5 @@ end
 file_path = try ARGS[1] catch; "inputs/03-1" end
 data = read_file_lines(file_path)
 result = solve(data)
-@test result == 273
+@test result == 15622
 println(result)
