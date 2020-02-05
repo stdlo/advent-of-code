3e8bbb2d3588795f15dec9779108b17c51f3e239
Logan Bickmore <loganbickmore91@gmail.com>
2020-02-05 09:13:25 -0800

    working on 201501

--- 2015/01/solution.clj
+++ 2015/01/solution.clj
@@ -1,1 +1,11 @@
 (println "こんにちは Clojure!")
+(def cases [
+"(())" "()()" ; both result in floor 0.
+"(((" "(()(()(" ;both result in floor 3.
+"))(((((" ; also results in floor 3.
+"())" "))(" ; both result in floor -1 (the first basement level).
+")))" ")())())" ; both result in floor -3.
+])
+(defn solve [input] (println (reduce + (map #(if (= % \( ) 1 -1) input))))
+(doseq [case cases] (solve case))
+; (def input (slurp "input"))
