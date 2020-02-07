a47c8926e04662f7eaa62131517024b5147f6c2e
Logan Bickmore <loganbickmore91@gmail.com>
2020-02-07 15:39:23 -0800

    almost there

--- 2015/01/solution.clj
+++ 2015/01/solution.clj
@@ -1,11 +1,35 @@
-(println "こんにちは Clojure!")
+(require '[clojure.test :as test])
+
 (def cases [
-"(())" "()()" ; both result in floor 0.
-"(((" "(()(()(" ;both result in floor 3.
-"))(((((" ; also results in floor 3.
-"())" "))(" ; both result in floor -1 (the first basement level).
-")))" ")())())" ; both result in floor -3.
-])
-(defn solve [input] (println (reduce + (map #(if (= % \( ) 1 -1) input))))
-(doseq [case cases] (solve case))
-; (def input (slurp "input"))
+    [0 "(())"]
+    [0 "()()"]
+    [3 "((("]
+    [3 "(()(()("]
+    [3 "))((((("]
+    [-1 "())"]
+    [-1 "))("]
+    [-3 ")))"]
+    [-3 ")())())"]
+    ])
+
+; Solve function
+(defn solve_p1 [input] (reduce + (map #(if (= % \( ) 1 -1) input)))
+; reducer that counts iterations and stops when we hit -1 calculations
+; (defn reducer [acc, val] (if (= -1 (acc :floor)) (reduced acc) {:floor (+ val (acc :f)) :index (+ 1 (acc :index))}))
+(defn reducer [acc, val] {:floor (+ val (acc :f)) :index (+ 1 (acc :index))})
+; (reduce (fn [acc, val] {:f (+ val (acc :f)) :i (+ 1(acc :i))}) {:i -1 :f 0} [1 1 1 -1 -1 -1 -1])
+    ; (let [sum (+ (nth 1 a) b) i (nth 0 a)]
+        ; (println a b)
+        ; (if (> sum -1) (reduced sum) [sum])))
+
+(defn solve_p2 [input] (reduce reducer {:index -1 :floor 0} (map #(if (= % \( ) 1 -1) input)))
+
+; Define tests from given cases
+; (test/deftest test-cases (doseq [case cases] (test/is (= (nth case 0) (solve_p1 (nth case 1))))))
+
+; Redirect test output to stderr so you can directly copy the results of a successful run
+; (binding [test/*test-out* *err*] (def summary (test/run-tests)))
+
+; Check test results, if it was good run the main input file and spit the result to stdout
+; (if (test/successful? summary) (println (solve_p2 (slurp "input"))))
+(println (solve_p2 (slurp "input")))
