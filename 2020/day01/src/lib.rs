b84aba1560bbf3b1a9192a497c94a8e855e9c742
stdlo <loganbickmore91@gmail.com>
2023-08-02 15:32:21 -0700

    day01 done in the most naive possible way

--- 2020/day01/src/lib.rs
+++ 2020/day01/src/lib.rs
@@ -1,20 +1,59 @@
 pub fn process_part1(input: &str) -> String {
-    "it works".to_string()
+    let items = input.lines().map(|v| v.parse::<u32>().unwrap());
+    let mut result = 0;
+    for a in items.clone() {
+        for b in items.clone() {
+            if a + b == 2020 {
+                result = a * b;
+                break;
+            }
+        }
+        if result != 0 {
+            break;
+        }
+    }
+    result.to_string()
 }
 
 pub fn process_part2(input: &str) -> String {
-    "it works".to_string()
+    let items = input.lines().map(|v| v.parse::<u32>().unwrap());
+    let mut result = 0;
+    for a in items.clone() {
+        for b in items.clone() {
+            for c in items.clone() {
+                if a + b + c == 2020 {
+                    result = a * b * c;
+                    break;
+                }
+            }
+        }
+        if result != 0 {
+            break;
+        }
+    }
+    result.to_string()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    const INPUT: &str = "";
+    const INPUT: &str = "1721
+979
+366
+299
+675
+1456";
 
     #[test]
     fn part1() {
         let result = process_part1(INPUT);
-        assert_eq!(result, "it works");        
+        assert_eq!(result, "514579");
+    }
+
+    #[test]
+    fn part2() {
+        let result = process_part2(INPUT);
+        assert_eq!(result, "241861950");
     }
 }
